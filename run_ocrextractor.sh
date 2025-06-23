7a64c1f5221aeacc5cdbc384aa42d31dbb162649
abdulahad2307 <smaahad.bd@gmail.com>
2025-06-23 09:20:24 +0200

    ocr extractor script

---
+++ run_ocrextractor.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=ocr_extraction_all4         # Job name
+#SBATCH --output=logs/%x_%j.out           # Standard output log
+#SBATCH --error=logs/%x_%j.err            # Error log
+#SBATCH --partition=v100                  # GPU partition name
+#SBATCH --nodes=1                         # Number of nodes
+#SBATCH --ntasks=1                        # Number of tasks
+#SBATCH --cpus-per-task=1                 # Number of CPU cores per task
+#SBATCH --gres=gpu:v100:1                 # Number of GPUs
+#SBATCH --time=23:55:00                   # Time limit hrs:min:sec
+#SBATCH --export=NONE                     # Avoid inheriting unwanted environment variables
+
+unset SLURM_EXPORT_ENV
+
+module load cuda/12.6
+module load python/3.12-conda
+conda activate ocr_env
+
+export http_proxy=http://proxy:80
+export https_proxy=http://proxy:80
+export PYTHONPATH=$PYTHONPATH:$(pwd)/FAU-Masters_Thesis-Ahad
+
+echo "Starting OCR Extraction"
+
+DATA_DIR="/home/woody/iwi5/iwi5280h/dataset/prepdata"
+OUTPUT_JSON="/home/woody/iwi5/iwi5280h/dataset/all_dataset_ocr_texts_pero4.json"
+OCR_ENGINE="pero"
+MAX_SIZE=1024
+
+python utils/ocr_extraction.py \
+  --data_dir $DATA_DIR \
+  --output_json $OUTPUT_JSON \
+  --max_images 100000 \
+  --offset 300000
+
+
+echo "OCR Extraction Completed. Output: $OUTPUT_JSON"
+
+#sbatch run_ocrextractor.sh
+#--data_dir /home/woody/iwi5/iwi5280h/dataset/prepdata \
+#--data_dir /home/woody/iwi5/iwi5280h/dataset/small_dataset \
